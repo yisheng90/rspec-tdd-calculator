d4328b7cd6324435dd7bf9d34a34d82ee51fd7ac
Yi Sheng Lee <yishenglee@Yis-MacBook-Pro.local>
2017-02-01 13:00:10 +0800

    test result

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,3 +1,7 @@
 class Calculator
   # your class goes here
+  def initialize num
+    @result = num
+  end
+
 end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,5 +1,5 @@
 require_relative 'spec_helper'
-require_relative '../lib/item'
+require_relative '../lib/calculator'
 
 describe Calculator do
   before(:context) do
@@ -11,6 +11,21 @@ describe Calculator do
   describe "This is the initiailization part" do
     it "is an instance of the Calculator class" do
       expect(@my_calculator).to be_instance_of(Calculator)
+    end
+  end
+
+  # Test for results
+  describe "This is to test the result method" do
+    it "should return the current result" do
+      expect(@my_calculator.result).to eq(1)
+    end
+
+    before(:context) do
+      @my_calculator.result  = 2
+    end
+
+    it "current result must be read only" do
+      expect(@my_calculator.result).to eq(1)
     end
   end
 end
